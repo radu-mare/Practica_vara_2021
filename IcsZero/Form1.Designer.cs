af28a7bb414fbef290c83f61dfd0f6f2f72451a1
radu-mare <73108706+radu-mare@users.noreply.github.com>
2021-07-09 21:15:24 +0300

    Practica_vara_2021

---
+++ IcsZero/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace IcsZero
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Lime;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(167, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 48);
+            this.label1.TabIndex = 0;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Lime;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(222, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 48);
+            this.label2.TabIndex = 1;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Lime;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(278, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 48);
+            this.label3.TabIndex = 2;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Lime;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(167, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 48);
+            this.label4.TabIndex = 5;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Lime;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(222, 176);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 48);
+            this.label5.TabIndex = 4;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Lime;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(278, 176);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 48);
+            this.label6.TabIndex = 3;
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Lime;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(166, 233);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 48);
+            this.label7.TabIndex = 8;
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Lime;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(222, 233);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(50, 48);
+            this.label8.TabIndex = 7;
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.Lime;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(278, 233);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(50, 48);
+            this.label9.TabIndex = 6;
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 566);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ IcsZero/Form1.cs
@@ -0,0 +1,616 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IcsZero
+{
+    public partial class Form1 : Form
+    {
+        bool b = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (label1.Text == "")
+                if (b)
+                {
+                    label1.Text = "0";
+                    b = !b;
+                }
+                else
+                {
+                    label1.Text = "1";
+                    b = !b;
+                }
+            if (label1.Text == label2.Text && label2.Text == label3.Text)
+            {
+                if (label1.Text == "0")
+                {
+                    MessageBox.Show("A castigat Toni");
+                }
+                else MessageBox.Show("A castigat Dorel");
+
+                label1.Text = "";
+                label2.Text = "";
+                label3.Text = "";
+            
+                label4.Text = "";
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "";
+                label8.Text = "";
+                label9.Text = "";
+            }
+            if (label1.Text == label4.Text && label4.Text == label7.Text)
+            {
+                if (label1.Text == "0")
+                {
+                    MessageBox.Show("A castigat Toni");
+                }
+                else MessageBox.Show("A castigat Dorel");
+                label1.Text = "";
+
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "";
+
+                label8.Text = "";
+                label9.Text = "";
+            }
+            if (label1.Text == label5.Text && label5.Text == label9.Text)
+            {
+                if (label1.Text == "0")
+                {
+                    MessageBox.Show("A castigat Toni");
+                }
+                else MessageBox.Show("A castigat Dorel");
+                label1.Text = "";
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "";
+                label8.Text = "";
+                label9.Text = "";
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            if (label2.Text == "")
+                if (b)
+            {
+                label2.Text = "0";
+                b = !b;
+            }
+            else
+            {
+                label2.Text = "1";
+                b = !b;
+            }
+
+            if (label1.Text == label2.Text && label2.Text == label3.Text)
+            {
+                if (label1.Text == "0") MessageBox.Show("A castigat Toni");
+                else MessageBox.Show("A castigat Dorel");
+                label1.Text = "";
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "";
+                label8.Text = "";
+                label9.Text = "";
+            }
+            if (label2.Text == label5.Text && label5.Text == label8.Text)
+            {
+                if (label2.Text == "0") MessageBox.Show("A castigat Toni");
+                else MessageBox.Show("A castigat Dorel");
+                  label1.Text = "";
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "";
+                label8.Text = "";
+                label9.Text = "";
+            }
+          
+        }
+
+        // TODO faceti restul sa mearga :(
+        private void label3_Click(object sender, EventArgs e)
+        {
+            if (label3.Text == "")
+                if (b)
+            {
+                label3.Text = "0";
+                b = !b;
+            }
+            else
+            {
+                label3.Text = "1";
+                b = !b;
+            }
+            if (label1.Text == label2.Text && label2.Text == label3.Text)
+            {
+                if (label1.Text == "0") MessageBox.Show("A castigat Toni");
+                else MessageBox.Show("A castigat Dorel");
+                label1.Text = "";
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "";
+                label8.Text = "";
+                label9.Text = "";
+            }
+
+            if (label3.Text == label5.Text && label5.Text == label7.Text)
+            {
+                if (label3.Text == "0")
+                {
+                    MessageBox.Show("A castigat Toni");
+                }
+                else MessageBox.Show("A castigat Dorel");
+                label1.Text = "";
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "";
+                label8.Text = "";
+                label9.Text = "";
+            }
+
+            if (label3.Text == label6.Text && label6.Text == label9.Text)
+            {
+                if (label3.Text == "0")
+                {
+                    MessageBox.Show("A castigat Toni");
+                }
+                else MessageBox.Show("A castigat Dorel");
+                label1.Text = "";
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "";
+                label8.Text = "";
+                label9.Text = "";
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            if (label6.Text == "")
+                if (b)
+            {
+                label6.Text = "0";
+                b = !b;
+            }
+            else
+            {
+                label6.Text = "1";
+                b = !b;
+            }
+
+            if (label4.Text == label5.Text && label5.Text == label6.Text)
+            {
+                if (label4.Text == "0")
+                {
+                    MessageBox.Show("A castigat Toni");
+                }
+                else MessageBox.Show("A castigat Dorel");
+                label1.Text = "";
+
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "";
+
+                label8.Text = "";
+                label9.Text = "";
+            }
+
+
+            if (label3.Text == label6.Text && label6.Text == label9.Text)
+            {
+                if (label3.Text == "0")
+                {
+                    MessageBox.Show("A castigat Toni");
+                }
+                else MessageBox.Show("A castigat Dorel");
+                label1.Text = "";
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "";
+                label8.Text = "";
+                label9.Text = "";
+            }
+
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            if (label5.Text == "")
+                if (b)
+            {
+                label5.Text = "0";
+                b = !b;
+            }
+            else
+            {
+                label5.Text = "1";
+                b = !b;
+            }
+
+            if (label2.Text == label5.Text && label5.Text == label8.Text)
+            {
+                if (label2.Text == "0") MessageBox.Show("A castigat Toni");
+                else MessageBox.Show("A castigat Dorel");
+                label1.Text = "";
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "";
+                label8.Text = "";
+                label9.Text = "";
+            }
+
+            if (label1.Text == label5.Text && label5.Text == label9.Text)
+            {
+                if (label1.Text == "0") MessageBox.Show("A castigat Toni");
+                else MessageBox.Show("A castigat Dorel");
+                label1.Text = "";
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "";
+                label8.Text = "";
+                label9.Text = "";
+            }
+
+            if (label3.Text == label5.Text && label5.Text == label7.Text)
+            {
+                if (label3.Text == "0")
+                {
+                    MessageBox.Show("A castigat Toni");
+                }
+                else MessageBox.Show("A castigat Dorel");
+                label1.Text = "";
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "";
+                label8.Text = "";
+                label9.Text = "";
+            }
+
+            if (label4.Text == label5.Text && label5.Text == label6.Text)
+            {
+                if (label4.Text == "0")
+                {
+                    MessageBox.Show("A castigat Toni");
+                }
+                else MessageBox.Show("A castigat Dorel");
+                label1.Text = "";
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "";
+                label8.Text = "";
+                label9.Text = "";
+            }
+
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            if (label4.Text == "")
+                if (b)
+            {
+                label4.Text = "0";
+                b = !b;
+            }
+            else
+            {
+                label4.Text = "1";
+                b = !b;
+            }
+
+            if (label1.Text == label4.Text && label4.Text == label7.Text)
+            {
+                if (label1.Text == "0")
+                {
+                    MessageBox.Show("A castigat Toni");
+                }
+                else MessageBox.Show("A castigat Dorel");
+                label1.Text = "";
+
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "";
+
+                label8.Text = "";
+                label9.Text = "";
+            }
+
+            if (label4.Text == label5.Text && label5.Text == label6.Text)
+            {
+                if (label4.Text == "0")
+                {
+                    MessageBox.Show("A castigat Toni");
+                }
+                else MessageBox.Show("A castigat Dorel");
+                label1.Text = "";
+
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "";
+
+                label8.Text = "";
+                label9.Text = "";
+            }
+
+
+
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            if (label9.Text == "")
+                if (b)
+            {
+                label9.Text = "0";
+                b = !b;
+            }
+            else
+            {
+                label9.Text = "1";
+                b = !b;
+            }
+
+            if (label7.Text == label8.Text && label8.Text == label9.Text)
+            {
+                if (label7.Text == "0")
+                {
+                    MessageBox.Show("A castigat Toni");
+                }
+                else MessageBox.Show("A castigat Dorel");
+                label1.Text = "";
+
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "";
+
+                label8.Text = "";
+                label9.Text = "";
+            }
+
+            if (label1.Text == label5.Text && label5.Text == label9.Text)
+            {
+                if (label1.Text == "0")
+                {
+                    MessageBox.Show("A castigat Toni");
+                }
+                else MessageBox.Show("A castigat Dorel");
+                label1.Text = "";
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "";
+                label8.Text = "";
+                label9.Text = "";
+            }
+
+            if (label3.Text == label6.Text && label6.Text == label9.Text)
+            {
+                if (label3.Text == "0")
+                {
+                    MessageBox.Show("A castigat Toni");
+                }
+                else MessageBox.Show("A castigat Dorel");
+                label1.Text = "";
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "";
+                label8.Text = "";
+                label9.Text = "";
+            }
+
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            if (label8.Text == "")
+                if (b)
+            {
+                label8.Text = "0";
+                b = !b;
+            }
+            else
+            {
+                label8.Text = "1";
+                b = !b;
+            }
+
+            if (label2.Text == label5.Text && label5.Text == label8.Text)
+            {
+                if (label2.Text == "0") MessageBox.Show("A castigat Toni");
+                else MessageBox.Show("A castigat Dorel");
+                label1.Text = "";
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "";
+                label8.Text = "";
+                label9.Text = "";
+            }
+
+            if (label7.Text == label8.Text && label8.Text == label9.Text)
+            {
+                if (label7.Text == "0")
+                {
+                    MessageBox.Show("A castigat Toni");
+                }
+                else MessageBox.Show("A castigat Dorel");
+                label1.Text = "";
+
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "";
+
+                label8.Text = "";
+                label9.Text = "";
+            }
+
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            if (label7.Text == "")
+                if (b)
+            {
+                label7.Text = "0";
+                b = !b;
+            }
+            else
+            {
+                label7.Text = "1";
+                b = !b;
+            }
+
+            if (label3.Text == label5.Text && label5.Text == label7.Text)
+            {
+                if (label3.Text == "0")
+                {
+                    MessageBox.Show("A castigat Toni");
+                }
+                else MessageBox.Show("A castigat Dorel");
+                label1.Text = "";
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "";
+                label8.Text = "";
+                label9.Text = "";
+            }
+            if (label1.Text == label4.Text && label4.Text == label7.Text)
+            {
+                if (label1.Text == "0")
+                {
+                    MessageBox.Show("A castigat Toni");
+                }
+                else MessageBox.Show("A castigat Dorel");
+                label1.Text = "";
+
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "";
+
+                label8.Text = "";
+                label9.Text = "";
+            }
+
+            if (label7.Text == label8.Text && label8.Text == label9.Text)
+            {
+                if (label7.Text == "0")
+                {
+                    MessageBox.Show("A castigat Toni");
+                }
+                else MessageBox.Show("A castigat Dorel");
+                label1.Text = "";
+
+                label2.Text = "";
+                label3.Text = "";
+                label4.Text = "";
+
+                label5.Text = "";
+                label6.Text = "";
+                label7.Text = "";
+
+                label8.Text = "";
+                label9.Text = "";
+            }
+        }
+    }
+}
